32d998f03f82db98834008f8c746fdba107aa645
rifqibaba <82257016+rifqibaba@users.noreply.github.com>
2022-11-20 08:35:01 +0700

    edit number

--- config.js
+++ config.js
@@ -2,9 +2,7 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['6282391261853', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 mods = [] // Want some help?
